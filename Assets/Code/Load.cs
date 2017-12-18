0111a4f29da587b5f9dad8615665f32c1b1fdf0f
e1500844 <e1500844@edu.vamk.fi>
2017-12-19 00:25:51 +0200

    proto3

---
+++ Assets/Code/Load.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleJSON;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking; 
+using System.Collections;
+
+public class Load : MonoBehaviour
+{
+	Model[] modelParameters;
+    List<GameObject> models = new List<GameObject>();
+    GameObject instance;
+	bool updateStatus = false;
+
+	string data = "";
+
+    void Start()
+    {
+        StartCoroutine(WaitForRequest());
+	}
+
+	void LateUpdate()
+	{
+	}
+
+	void Update(){
+		if (updateStatus == true) 
+		{
+			updateStatus = false;
+			DeleteAll ();
+			StartCoroutine(WaitForRequest());
+		}
+		if (Input.GetKeyDown(KeyCode.Q)) {
+            DeleteAll();
+            StartCoroutine(WaitForRequest());
+		}
+			
+	}
+
+    private void DeleteAll()
+    {
+        foreach (GameObject g in models)
+        {
+            GameObject.Destroy(g);
+        }
+    }
+
+    IEnumerator WaitForRequest()
+    {
+		string fixJson = "http://www.cc.puv.fi/~e1500844/cord.json";
+		string modelJson = "http://www.cc.puv.fi/~e1401150/3dmodel.json";
+
+		WWW www = new WWW (fixJson);
+
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+            Debug.Log(www.text);
+           
+            string jono = www.text;
+
+            int count = jono.Split('x').Length - 1;
+
+			modelParameters = Model.getModelParameter (jono, count);
+
+			Debug.Log(modelParameters[0].Y);
+
+            for (int i = 0; i < count; i++) 
+			{
+				//Hakee Resources kansiosta jsonissa määritellyn 3d mallin cubes[i].Type
+				instance = Instantiate (Resources.Load (modelParameters[i].Type, typeof(GameObject))) as GameObject;
+
+				instance.AddComponent<BoxCollider> ();
+				instance.AddComponent<Rigidbody> ();
+
+				//Asettaa objektin sijainnin
+				Vector3 v = new Vector3 (modelParameters[i].X, modelParameters[i].Y, modelParameters[i].Z);
+				instance.transform.position = v;
+
+				//Hakee Jsonista värin ja asettaa sen Objektille
+				Color MyColour = Color.clear; ColorUtility.TryParseHtmlString (modelParameters [i].Color, out MyColour);
+				instance.GetComponentInChildren<Renderer> ().material.color = MyColour;
+
+				//Lisää 3d mallit listaan, käytetään mallien poistamisessa maailmasta
+                models.Add(instance);
+			}
+
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    
+    }
+}
---
+++ Assets/Code/Model.cs
@@ -0,0 +1,114 @@
+﻿using SimpleJSON;
+using UnityEngine;
+
+class Model
+{	
+	float x;
+	float y;
+	float z;
+	string color;
+	string type;
+	int angle;
+
+	public Model(float x, float y, float z, string color, string type, int angle)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.color = color;
+		this.type = type;
+		this.angle = angle;
+	}
+
+	public float X
+	{
+		get
+		{
+			return x;
+		}
+
+		set
+		{
+			x = value;
+		}
+	}
+
+	public float Y
+	{
+		get
+		{
+			return y;
+		}
+
+		set
+		{
+			y = value;
+		}
+	}
+
+	public float Z
+	{
+		get
+		{
+			return z;
+		}
+
+		set
+		{
+			z = value;
+		}
+	}
+
+	public string Color
+	{
+		get
+		{
+			return color;
+		}
+
+		set
+		{
+			color = value;
+		}
+	}
+
+	public string Type
+	{
+		get
+		{
+			return type;
+		}
+
+		set
+		{
+			type = value;
+		}
+	}
+
+	public int Angle
+	{
+		get
+		{
+			return angle;
+		}
+
+		set
+		{
+			angle = value;
+		}
+	}
+
+	public static Model[] getModelParameter(string jono, int amount){
+		
+		Model[] cubes = new Model[amount];
+		var N = JSON.Parse(jono);
+
+		for (int i = 0; i < amount; i++) 
+		{
+			cubes [i] = new Model (N [i] ["x"].AsFloat, N [i] ["y"].AsFloat, N [i] ["z"].AsFloat, N [i] ["color"], N [i] ["type"], N [i] ["angle"].AsInt);
+		}
+
+		return cubes;
+	}
+
+}
